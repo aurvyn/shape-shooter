cd1738dfebc2de1a5017c15c080bbf9262aab109
Aurvyn <admin@dysrakescence.eu.org>
2025-04-27 20:39:02 -0400

    ADD: speedster and final boss + better game over screen

--- app/Main.hs
+++ app/Main.hs
@@ -45,6 +45,7 @@ data GameState = GameState {
     enemies :: [Entity],
     eBullets :: [Entity],
     currentWeapon :: String,
+    timeLapsed :: Float,
     isPaused :: Bool
 }
 
@@ -164,30 +165,63 @@ roombaPlayerPlan = repeatedlyShootRandom
 playerBomb :: Entity
 playerBomb = playerColor bomb{ vel = (0, 150), action = moveUntil 0.5 $ bombPlan playerBomb 4.9 }
 
+mothershipBomb :: Entity
+mothershipBomb = enemyColor bomb{ action = moveUntil 2 $ bombPlan mothershipBomb 6.9, mesh = color yellow $ mesh bomb, damage = 10 }
+
 bombPlayerPlan :: Action
 bombPlayerPlan = repeatedlyShoot playerBomb 1.0 1.0 idle
 
 gruntPlan :: Float -> Bool -> Action
 gruntPlan speed isLeft
-    = moveTo (sign*200, 200) 100
-    $ spreadShoot (enemyColor rubber) 0.2
-    $ moveTo (sign*250, 100) 100
-    $ wait (1.0/speed)
-    $ spreadShoot (enemyColor rubber) 0.2
-    $ moveTo (sign*200, 0) 100
-    $ wait (1.0/speed)
-    $ spreadShoot (enemyColor rubber) 0.2
-    $ moveTo (sign*150, -100) 100
-    $ wait (1.0/speed)
-    $ spreadShoot (enemyColor rubber) 0.2
-    $ moveTo (-sign*150, -450) 100
+    = moveTo (sign*200, 200) moveSpeed
+    $ spreadShootMove ammo 0.3 (250, 100)
+    $ spreadShootMove ammo 0.25 (200, 0)
+    $ spreadShootMove ammo 0.2 (150, -100)
+    $ spreadShootMove ammo 0.15 (-150, 450)
     despawn
     where
+        moveSpeed = 200 * speed
+        ammo = enemyColor rubber{ damage = 5 }
         sign = if isLeft then -1 else 1
-        spreadShoot bullet time nextAction = shootWait bullet{ vel = (-sign*50*speed, 80*speed) } time
+        spreadShoot bullet time nextAction =
+            shootWait bullet{ vel = (-sign*50*speed, 80*speed) } time
             $ shootWait bullet{ vel = (-sign*100*speed, 0) } time
             $ shootWait bullet{ vel = (-sign*50*speed, -80*speed) } time
             $ shoot bullet{ vel = (-sign*10*speed, -100*speed) } nextAction
+        spreadShootMove bullet time (x, y) nextAction =
+            spreadShoot bullet time
+            $ moveTo (sign*x, y) moveSpeed
+            $ wait (1.0/speed) nextAction
+
+speedsterPlan :: Float -> Bool -> Action
+speedsterPlan speed isLeft
+    = moveTo (sign*200, 200) moveSpeed
+    $ spreadShootMove ammo 0.3 (200, 200)
+    $ spreadShootMove ammo 0.25 (150, 50)
+    $ spreadShootMove ammo 0.2 (100, -100)
+    $ spreadShootMove ammo 0.15 (-100, -450)
+    despawn
+    where
+        moveSpeed = 200 * speed
+        ammo = enemyColor rubber{ damage = 3 }
+        sign = if isLeft then -1 else 1
+        spreadShoot bullet time nextAction =
+            shootWait bullet{ vel = (-sign*60*speed, 90*speed) } time
+            $ shootWait bullet{ vel = (-sign*120*speed, 0) } time
+            $ shootWait bullet{ vel = (-sign*60*speed, -100*speed) } time
+            $ shootWait bullet{ vel = (-sign*20*speed, -10*speed) } time
+            $ shootWait bullet{ vel = (sign*20*speed, -110*speed) } time
+            $ shoot bullet{ vel = (-sign*10*speed, -100*speed) } nextAction
+        spreadShootMove bullet time (x, y) nextAction =
+            spreadShoot bullet time
+            $ moveTo (sign*x, y) moveSpeed
+            $ wait (1.0/speed) nextAction
+
+mothershipPlan :: Action
+mothershipPlan
+    = moveTo (0, 300) 50
+    $ repeatedlyShootRandom (1.2*pi, 1.8*pi) mothershipBomb 50 1 2
+    $ idle
 
 grunt :: Entity
 grunt = Entity
@@ -200,6 +234,30 @@ grunt = Entity
     10
     idle
     $ color red $ rectangleSolid 50 50
+
+speedster :: Entity
+speedster = Entity
+    (0, 0)
+    (0, 0)
+    15
+    30
+    30
+    2
+    15
+    idle
+    $ color (dark orange) $ rectangleSolid 30 30
+
+mothership :: Entity
+mothership = Entity
+    (0, 0)
+    (0, 0)
+    50
+    1000
+    1000
+    3
+    50
+    mothershipPlan
+    $ color (dark yellow) $ rectangleSolid 100 100
 
 rubber :: Entity
 rubber = Entity
@@ -259,29 +317,58 @@ initialState = GameState {
     enemies = [
         grunt { pos = (0, 500), action = gruntPlan 1 True },
         grunt { pos = (0, 500), action = wait 2.0 $ gruntPlan 1 False },
-        grunt { pos = (200, 500), action = wait 5.0 $ gruntPlan 1.2 True },
-        grunt { pos = (-400, 500), action = wait 7.0 $ gruntPlan 1.2 False },
-        grunt { pos = (-400, 400), action = wait 10.0 $ gruntPlan 1.5 True },
-        grunt { pos = (400, 400), action = wait 12.0 $ gruntPlan 1.5 False }
+        grunt { pos = (200, 500), action = wait 7.0 $ gruntPlan 1.2 True },
+        grunt { pos = (-400, 500), action = wait 10.0 $ gruntPlan 1.2 False },
+        grunt { pos = (-400, 400), action = wait 15.0 $ gruntPlan 1.5 True },
+        grunt { pos = (400, 400), action = wait 17.0 $ gruntPlan 1.5 False },
+        speedster { pos = (0, 500), action = wait 25.0 $ speedsterPlan 1 True },
+        speedster { pos = (0, 500), action = wait 26.0 $ speedsterPlan 1 False },
+        speedster { pos = (200, 500), action = wait 30.0 $ speedsterPlan 1.2 True },
+        speedster { pos = (-400, 500), action = wait 31.0 $ speedsterPlan 1.2 False },
+        speedster { pos = (-400, 400), action = wait 37.0 $ speedsterPlan 1.5 True },
+        speedster { pos = (400, 400), action = wait 40.0 $ speedsterPlan 1.5 False },
+        mothership { pos = (0, 600), action = wait 45.0 $ mothershipPlan }
     ],
     eBullets = [],
     currentWeapon = "Rubber Gun",
+    timeLapsed = 0,
     isPaused = False
 }
 
 render :: GameState -> IO Picture
-render (GameState player@(Entity _ _ _ pMaxHP pHP _ score _ _) pBullets enemies eBullets weapon isPaused) =
-    pure $ if pHP <= 0 then
-        pictures [
-            translate (-200) 50
-                $ scale 0.5 0.5
-                $ color white
-                $ text "Game Over!",
-            translate (-200) (-50)
-                $ scale 0.3 0.3
-                $ color yellow
-                $ text ("Score: " ++ show score)
-        ]
+render (GameState player@(Entity _ _ _ pMaxHP pHP _ score _ _) pBullets enemies eBullets weapon timeLapsed isPaused) =
+    pure $ if pHP <= 0 || null enemies then let
+        titlePic = translate (-200) 100
+            $ scale 0.5 0.5
+            $ if null enemies then
+                color (light $ light blue)
+                $ text "You Won!"
+            else
+                color white
+                $ text "Game Over!"
+        timeScore = 100 - round timeLapsed :: Int
+        healthScore = max 0 pHP
+        killScorePic = translate (-180) (0)
+            $ scale 0.2 0.2
+            $ color (dark yellow)
+            $ text ("Kill Score: " ++ show score)
+        timeScorePic = translate (-180) (-50)
+            $ scale 0.2 0.2
+            $ color (dark yellow)
+            $ text ("Time Score: " ++ show timeScore)
+        healthScorePic = translate (-180) (-100)
+            $ scale 0.2 0.2
+            $ color (dark yellow)
+            $ text ("Health remaining: " ++ show healthScore)
+        totalScorePic = translate (-180) (-160)
+            $ scale 0.3 0.3
+            $ color yellow
+            $ text ("Total Score: " ++ show (score + timeScore + healthScore))
+        restartPic = translate (-100) (-250)
+            $ scale 0.15 0.15
+            $ color green
+            $ text "Press 'n' to restart"
+        in pictures [titlePic, killScorePic, timeScorePic, healthScorePic, totalScorePic, restartPic]
     else let
         entityPics = pictures [
             translate x y mesh
@@ -321,7 +408,7 @@ tick dt entity@Entity{ pos = (x, y), vel = (vx, vy), action = Action action } =
     action dt entity{ pos = (x + vx*dt, y + vy*dt) }
 
 tickScene :: Float -> GameState -> IO GameState
-tickScene dt (GameState player pBullets enemies eBullets weapon isPaused) = do
+tickScene dt (GameState player pBullets enemies eBullets weapon timeLapsed isPaused) = do
     pEntities <- tick dt player
     let (player', addedPBullets) = fromMaybe (error "Player entity should not be deleted!") $ uncons pEntities
     pLists <- mapM (tick dt) pBullets
@@ -330,7 +417,8 @@ tickScene dt (GameState player pBullets enemies eBullets weapon isPaused) = do
     let (enemies', addedEBullets) = unzip $ mapMaybe uncons eLists
     eBulletLists <- mapM (tick dt) eBullets
     let eBullets' = concat addedEBullets ++ concat eBulletLists
-    pure $ GameState player' pBullets' enemies' eBullets' weapon isPaused
+    let timeLapsed' = timeLapsed + dt
+    pure $ GameState player' pBullets' enemies' eBullets' weapon timeLapsed' isPaused
 
 isIntersect :: Entity -> Entity -> Bool
 isIntersect (Entity (shipX, shipY) _ shipRad _ _ _ _ _ _) (Entity (enemyX, enemyY) _ enemyRad _ _ _ _ _ _) =
@@ -345,14 +433,14 @@ hitBy enemies ship = (enemies', ship') where
     enemies' = hitEnemies ++ misses
 
 collideShips :: GameState -> GameState
-collideShips (GameState player pBullets enemies eBullets weapon isPaused) = let
+collideShips (GameState player pBullets enemies eBullets weapon timeLapsed isPaused) = let
     (eBullets', newPlayer) = hitBy eBullets player
     (newEnemies, player') = hitBy enemies newPlayer
     (pBullets', enemies') = mapAccumL hitBy pBullets newEnemies
-    in GameState player' pBullets' enemies' eBullets' weapon isPaused
+    in GameState player' pBullets' enemies' eBullets' weapon timeLapsed isPaused
 
 despawnEntities :: GameState -> GameState
-despawnEntities (GameState player pBullets enemies eBullets weapon isPaused) = let
+despawnEntities (GameState player pBullets enemies eBullets weapon timeLapsed isPaused) = let
     isInside :: Entity -> Bool
     isInside Entity{ health, pos = (x, y) } =
         health > 0 &&
@@ -364,11 +452,11 @@ despawnEntities (GameState player pBullets enemies eBullets weapon isPaused) = l
     pBullets' = filter isInside pBullets
     eBullets' = filter isInside eBullets
     (enemies', deadEnemies) = partition ((> 0) . health) enemies
-    in GameState player' pBullets' enemies' eBullets' weapon isPaused
+    in GameState player' pBullets' enemies' eBullets' weapon timeLapsed isPaused
 
 update :: Float -> GameState -> IO GameState
 update seconds_lapsed game
-    | isPaused game || health (player game) < 0 = pure game
+    | isPaused game || health (player game) < 0 || null (enemies game) = pure game
     | otherwise = do
         game' <- tickScene seconds_lapsed game
         pure $ despawnEntities . collideShips $ game'
